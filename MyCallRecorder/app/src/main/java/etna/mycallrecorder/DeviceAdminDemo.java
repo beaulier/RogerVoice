e0d2c26efdaf54386a09a765df571fd81c2b05d4
beaulier <beaulier.thomas@gmail.com>
2015-12-14 13:37:05 +0100

    DeviceAdminDemo.java

---
+++ MyCallRecorder/app/src/main/java/etna/mycallrecorder/DeviceAdminDemo.java
@@ -0,0 +1,21 @@
+package etna.mycallrecorder;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Tom on 09/12/2015.
+ */
+public class DeviceAdminDemo extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    public void onEnabled(Context context, Intent intent) {
+    };
+
+    public void onDisabled(Context context, Intent intent) {
+    };
+}
